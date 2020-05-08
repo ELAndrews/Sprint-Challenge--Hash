10949030e50bff56b8bb062a62cc82b4c52ecb5a
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2020-05-08 10:03:13 +0100

    Complete ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -4,11 +4,43 @@ class Ticket:
         self.source = source
         self.destination = destination
 
+table = {}
 
 def reconstruct_trip(tickets, length):
     
     """
     YOUR CODE HERE
     """
+    route = []
+
+    for i in range(length):
+        table[tickets[i].source] = tickets[i].destination
+        i += 1
+
+    curr = table["NONE"]
+
+    while curr is not "NONE":
+        route.append(curr)
+        curr = table[curr]
+    
+    route.append(curr)
 
     return route
+
+
+ticket_1 = Ticket("PIT", "ORD")
+ticket_2 = Ticket("XNA", "SAP")
+ticket_3 = Ticket("SFO", "BHM")
+ticket_4 = Ticket("FLG", "XNA")
+ticket_5 = Ticket("NONE", "LAX")
+ticket_6 = Ticket("LAX", "SFO")
+ticket_7 = Ticket("SAP", "SLC")
+ticket_8 = Ticket("ORD", "NONE")
+ticket_9 = Ticket("SLC", "PIT")
+ticket_10 = Ticket("BHM", "FLG")
+
+tickets = [ticket_1, ticket_2, ticket_3, ticket_4, ticket_5,
+            ticket_6, ticket_7, ticket_8, ticket_9, ticket_10]
+
+
+print(reconstruct_trip(tickets, 10))
