43e6e55fb965745a6adbd51b4d26c19e53d76790
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2020-05-08 08:29:31 +0100

    Complete ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,21 @@
-def get_indices_of_item_weights(weights, length, limit):
+table = {}
 
+def get_indices_of_item_weights(weights, length, limit):
+    
     """
     YOUR CODE HERE
     """
-
+    ## weight of 2 which equals limit
+    ## limit - weight = required pair
+    ## pair in table? no? add weight to table yes? return value indices 
+    for i in range(length):
+        required = limit - weights[i]
+        if required in table.values():
+            return [i, weights.index(required)]
+        else:
+            table[i] = weights[i]
+            i += 1
     return None
+
+weights_3 = [4, 6, 10, 15, 16]
+print(get_indices_of_item_weights(weights_3, 5, 21))
