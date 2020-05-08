ea76b0c66d6805de6c5a80bbbab5cf8bf164c068
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2020-05-08 12:18:54 +0100

    complete ex4 - stretch

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -4,6 +4,16 @@ def has_negatives(a):
     YOUR CODE HERE
     """
 
+    table = {}
+
+    result = []
+
+    for i in a:
+        if abs(i) in table and abs(i) not in result:
+            result.append(abs(i))
+        table[abs(i)] = abs(i)
+        i += 1
+
     return result
 
 
