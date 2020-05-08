3d445dbd1a67e78268fd31a39eefb6a7befbef7e
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2020-05-08 11:36:38 +0100

    update ex3

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -8,43 +8,44 @@ def intersection(arrays):
 
     table = {}
 
+    for i in range(len(arrays[0])):
+        table[arrays[0][i]] = 0
+        i += 1
+    
     for arr in arrays:
         for i in arr:
-            if i in table:
+            if i in table.keys():
                 table[i] += 1
-            else:
-                table[i] = 1
 
     for i, v in enumerate(table):
         if table[v] == len(arrays):
             result.append(v)
 
-    # print(table, result)
-
     return result
 
 
-if __name__ == "__main__":
-    arrays = []
+# if __name__ == "__main__":
+#     arrays = []
 
-    arrays.append(list(range(1000000,2000000)) + [1,2,3])
-    arrays.append(list(range(2000000,3000000)) + [1,2,3])
-    arrays.append(list(range(3000000,4000000)) + [1,2,3])
+#     arrays.append(list(range(1000000,2000000)) + [1,2,3])
+#     arrays.append(list(range(2000000,3000000)) + [1,2,3])
+#     arrays.append(list(range(3000000,4000000)) + [1,2,3])
 
-    print(intersection(arrays))
+#     print(intersection(arrays))
 
 
-arrays = [
-            list(range(1000000, 2000000)) + [1,2,3],
-            list(range(2000000, 3000000)) + [1,2,3],
-            list(range(3000000, 4000000)) + [1,2,3],
-            list(range(4000000, 5000000)) + [1,2,3],
-            list(range(5000000, 6000000)) + [1,2,3],
-            list(range(6000000, 7000000)) + [1,2,3],
-            list(range(7000000, 8000000)) + [1,2,3],
-            list(range(8000000, 9000000)) + [1,2,3],
-            list(range(9000000, 10000000)) + [1,2,3],
-            list(range(10000000, 11000000)) + [1,2,3]
-        ]
+
+
+arrays = [ [1,2,3],
+            [1,4,5,3],
+            [1,6,7,3] ]
+
+# arrays = [
+#             list(range(1000000, 2000000)) + [1,2,3],
+#             list(range(7000000, 8000000)) + [1,2,3],
+#             list(range(8000000, 9000000)) + [1,2,3],
+#             list(range(9000000, 10000000)) + [1,2,3],
+#             list(range(10000000, 11000000)) + [1,2,3]
+#         ]
 
 print(intersection(arrays))
